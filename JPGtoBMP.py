76d58f93fc14060af2b6d1fb86fc919e610912e4
Thibault Huillet <thibault.huillet@telecom-paris.fr>
2020-11-04 08:28:46 +0100

    first commit

---
+++ JPGtoBMP.py
@@ -0,0 +1,8 @@
+from PIL import Image
+
+def convertToBMP(file): #file est un String du type "name.jpg"
+
+    img = Image.open(file)
+    img = img.save('%s.bmp' % file[:-4])
+
+    return('%s.bmp' % file[:-4])
