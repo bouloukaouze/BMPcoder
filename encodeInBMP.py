25c0de25538853669701247feab15ccda4f15b5d
Thibault Huillet <thibault.huillet@telecom-paris.fr>
2020-11-04 11:50:49 +0100

    là c'est plutôt bon

--- encodeInBMP.py
+++ encodeInBMP.py
@@ -1,7 +1,9 @@
 def encode(img, bitText): #img est de type np.array
 
     listBitText = list(str(bitText))
-    listBitText.append(list(str(100010101001110010001000100001101001111010001000100010101001000010001010101001001000101))) #code d'arrêt (en clair ENDCODEHERE)
+    endcode = str(100010101001110010001000100001101001111010001000100010101001000010001010101001001000101) #code d'arrêt (en clair ENDCODEHERE)
+    for char in list(endcode):
+        listBitText.append(char)
     listValues = []
     for line in img:
         for pixel in line:
@@ -11,7 +13,7 @@ def encode(img, bitText): #img est de type np.array
 
     for i in range(len(listBitText)):
         l = list(str(listValues[i]))
-        l[-1] = listBitText[i]
+        l[-1] = str(listBitText[i])
         listValues[i] = int(''.join(l))
 
     for line in img:
