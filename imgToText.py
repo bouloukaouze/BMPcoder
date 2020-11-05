39c3e64c3c7db0af2b240e837302bb7b249681bc
Clara <clara.delabrouille@gmail.com>
2020-11-05 15:35:32 +0100

    Correction texte clair vs binaire

--- imgToText.py
+++ imgToText.py
@@ -1,9 +1,13 @@
 def gettext(img):
     
+    """
+    Takes image with encoded message and returns 
+    """
+    
     addr_out=""
     message=""
     text=False
-    keyword="KEYWORD"
+    keyword="1000101010011100100010001000110010010010100110001000111" # binary keyword
     
     for line in img:
         for pixel in line:
@@ -14,7 +18,7 @@ def gettext(img):
                 else:
                     message+=bit
                 keyword=keyword[1:]+bit
-                if keyword=="ENDFILE":
+                if keyword=="1000101010011100100010001000110010010010100110001000101": # binary "ENDFILE"
                     text=True
-                if keyword=="ENDTEXT":
+                if keyword=="1000101010011100100010001010100010001010101100001010100": # binary "ENDTEXT"
                     return (addr_out, message)
