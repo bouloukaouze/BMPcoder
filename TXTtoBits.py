4dc5dd304cf27d9723b4cfc525a38f67205d934a
Thibault Huillet <thibault.huillet@telecom-paris.fr>
2020-11-05 08:31:00 +0100

    il n'y a plus l'en-tête de 'hex'

--- TXTtoBits.py
+++ TXTtoBits.py
@@ -15,7 +15,7 @@ def convertToBits(text):
 
 def convertToString(bin): #bin est un string du type '0b001011011110'
 
-    hex = hex(int(bin[2:], 2))
+    hex = hex(int(bin[2:], 2))[2:]
     text = codecs.decode(hex, 'hex').decode('utf-8')
     return(text)
 
