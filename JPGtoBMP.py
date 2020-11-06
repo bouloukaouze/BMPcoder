719e5d74e1bb11ab526c8fdd1a68ac48ef859fb1
Elikharyo <53236414+Elikharyo@users.noreply.github.com>
2020-11-06 11:34:18 +0100

    Delete JPGtoBMP.py

--- JPGtoBMP.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-def convertToBMP(file): #file est un string du type "name.jpg"
-
-    img = Image.open(file)
-    img = img.save('%s.bmp' % file[:-4])
-
-    return('%s.bmp' % file[:-4])
